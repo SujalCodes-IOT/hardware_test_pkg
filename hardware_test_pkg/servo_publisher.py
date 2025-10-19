78912a01a82442ed01a312d4062625274cda1b82
SujalCodes-IOT <sujaldhote82@gmail.com>
2025-10-19 22:18:36 +0530

    Updated servo_publisher to use /dev/gpiochip0

--- hardware_test_pkg/servo_publisher.py
+++ hardware_test_pkg/servo_publisher.py
@@ -1,16 +1,15 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
-import RPi.GPIO as GPIO
+import gpiod
 import time
 
-SERVO_PIN = 18
+SERVO_PIN = 18  # BCM pin number for servo
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
-
-pwm = GPIO.PWM(SERVO_PIN, 50)  # 50 Hz
-pwm.start(0)
+# Open GPIO chip
+chip = gpiod.Chip('gpiochip0')
+line = chip.get_line(SERVO_PIN)
+line.request(consumer="servo_test", type=gpiod.LINE_REQ_DIR_OUT)
 
 class ServoPublisher(Node):
     def __init__(self):
@@ -21,7 +20,7 @@ class ServoPublisher(Node):
         self.direction = 1
 
     def timer_callback(self):
-        # Sweep servo
+        # Sweep servo back and forth
         self.angle += 10 * self.direction
         if self.angle >= 180:
             self.angle = 180
@@ -30,14 +29,19 @@ class ServoPublisher(Node):
             self.angle = 0
             self.direction = 1
 
+        # Publish ROS 2 message
         msg = Int32()
         msg.data = self.angle
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing angle: {self.angle}')
 
-        # Set servo PWM
-        duty = 2 + (self.angle / 18)
-        pwm.ChangeDutyCycle(duty)
+        # Software PWM for servo using gpiod
+        period = 0.02  # 20 ms period (50 Hz)
+        high_time = 0.001 + (self.angle / 180.0) * 0.002  # 1–3 ms pulse
+        line.set_value(1)
+        time.sleep(high_time)
+        line.set_value(0)
+        time.sleep(period - high_time)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -47,8 +51,8 @@ def main(args=None):
     except KeyboardInterrupt:
         pass
     finally:
-        pwm.stop()
-        GPIO.cleanup()
+        # Release GPIO line and shutdown
+        line.release()
         node.destroy_node()
         rclpy.shutdown()
 
