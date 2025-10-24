9c1d8b5b3c6cdd7a0ca3648048416c591d8c38af
SujalCodes-IOT <sujaldhote82@gmail.com>
2025-10-24 18:04:28 +0530

    updated servo as a subscriber

--- hardware_test_pkg/servo_publisher.py
+++ hardware_test_pkg/servo_publisher.py
@@ -4,38 +4,29 @@ from std_msgs.msg import Int32
 import gpiod
 import time
 
-SERVO_PIN = 18  # BCM pin number for servo
+SERVO_PIN = 18  # BCM pin
 
-# Open GPIO chip
 chip = gpiod.Chip('gpiochip0')
 line = chip.get_line(SERVO_PIN)
-line.request(consumer="servo_test", type=gpiod.LINE_REQ_DIR_OUT)
+line.request(consumer="servo_driver", type=gpiod.LINE_REQ_DIR_OUT)
 
-class ServoPublisher(Node):
+class ServoDriver(Node):
     def __init__(self):
-        super().__init__('servo_publisher')
-        self.publisher_ = self.create_publisher(Int32, 'servo_angle', 10)
-        self.timer = self.create_timer(2.0, self.timer_callback)
-        self.angles = [0, 90, 180, 90]  # Sequence
-        self.index = 0
+        super().__init__('servo_driver')
+        self.subscription = self.create_subscription(Int32, 'servo_angle', self.listener_callback, 10)
+        self.subscription  # prevent unused variable warning
+        self.get_logger().info("Servo driver ready, waiting for commands...")
 
-    def timer_callback(self):
-        angle = self.angles[self.index]
-        self.index = (self.index + 1) % len(self.angles)
-
-        # Publish ROS 2 message
-        msg = Int32()
-        msg.data = angle
-        self.publisher_.publish(msg)
-        self.get_logger().info(f"Rotating servo to: {angle}°")
-
-        # Generate PWM pulse for servo
-        for _ in range(50):  # Run ~1 second of 50 Hz PWM
+    def listener_callback(self, msg):
+        angle = msg.data
+        self.get_logger().info(f"Received angle: {angle}°")
+        # Run ~1 second of PWM to move servo to target
+        for _ in range(50):
             self.set_servo_angle(angle)
 
     def set_servo_angle(self, angle):
-        period = 0.02  # 20 ms period (50 Hz)
-        high_time = 0.001 + (angle / 180.0) * 0.001  # 1–2 ms pulse width
+        period = 0.02  # 20ms
+        high_time = 0.001 + (angle / 180.0) * 0.001
         line.set_value(1)
         time.sleep(high_time)
         line.set_value(0)
@@ -43,7 +34,7 @@ class ServoPublisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ServoPublisher()
+    node = ServoDriver()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
